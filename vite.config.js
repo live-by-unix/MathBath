2567f0ba627785265f0eb3ec7c19310ef6a7a91c
live-by-unix <262261208+live-by-unix@users.noreply.github.com>
2026-06-19 10:59:24 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { base44 } from '@base44/sdk/vite';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
@@ -13,15 +12,6 @@ export default defineConfig({
   },
 
   plugins: [
-    base44({
-      legacySDKImports:
-        process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true,
-    }),
-
     react(),
   ],
 });
